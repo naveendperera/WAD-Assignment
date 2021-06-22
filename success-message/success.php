c98c18508363467cce78dd16082933a5ed79a547
nmnjeewanthi <maneeshanjeewanthi071@gmail.com>
2021-06-22 11:57:13 +0530

    successful message added

---
+++ success-message/success.php
@@ -0,0 +1,38 @@
+ <!DOCTYPE html>
+ <html>
+
+ <head>
+     <script>
+     function myFunction() {
+         thisUrl = new URLSearchParams(String(window.location))
+         var q = thisUrl.get('q')
+         document.getElementById("headertag").innerHTML = "Your Response" + " have been submitted";
+     }
+     $(window).ready(function() {
+         $('#loading').hide();
+     });
+     </script>
+     <style>
+     #loading {
+         background: url('success.gif') no-repeat center center;
+         position: absolute;
+         height: 100%;
+         width: 100%;
+     }
+     </style>
+ </head>
+
+ <body onload="myFunction()">
+     <div id="loading">
+         <center><br \><br \><br \><br \><br \><br \>
+             <h1 id="headertag" style="color: rgb(120, 178, 77);"><span id="headertag"></span></h1>
+         </center>
+     </div>
+
+ 
+
+
+
+ </body>
+
+ </html>
