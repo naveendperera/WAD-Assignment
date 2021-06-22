80740e6101eb07c71fe8cff03080c5b8913e8daf
nmnjeewanthi <maneeshanjeewanthi071@gmail.com>
2021-06-22 10:45:39 +0530

    connected with font awesome and >> style sheet

---
+++ contact-us.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Contact Us</title>
+</head>
+
+<body>
+ 
+
+</body>
+
+</html>
